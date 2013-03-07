2ad46580ae590ebc0ba3e3d760838e4639c3c96c
Austin Czarnecki <austin.czarnecki@yale.edu>
2013-03-06 21:17:17 -0500

    Work during class on edit and update commands. Added forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	# raise params.to_yaml #in order to see what's in the params hash, forces ruby to throw an error
+    @user = User.create(params[:user])
+    redirect_to users_path
+  end
+
+  def edit
+  	# @user = User.find()
+  	@user = User.find(params[:id])
+  end 
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update_attributes(params[:user])
+  	redirect_to users_path
+  end
+
 end
